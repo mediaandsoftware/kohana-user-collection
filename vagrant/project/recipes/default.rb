bd261f7614c49a60a7e592b7f5d935b0bf353baf
Paul Schwarz <paulsschwarz@gmail.com>
2012-07-10 08:10:46 +0300

    PECL Fileinfo claims that it is supported by default now. http://pecl.php.net/package/Fileinfo.

--- vagrant/project/recipes/default.rb
+++ vagrant/project/recipes/default.rb
@@ -21,7 +21,7 @@ require_recipe "apache2::mod_ssl"
 
 require_recipe "php"
 require_recipe "php::module_curl"
-require_recipe "php::module_fileinfo"
+# require_recipe "php::module_fileinfo"
 require_recipe "php::module_gd"
 require_recipe "php::module_memcache"
 require_recipe "php::module_mysql"
